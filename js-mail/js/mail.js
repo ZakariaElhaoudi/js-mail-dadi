9de3260102a4d318814cc22b1fbd4b0d62087ae8
Zakaria96 <zakarialavoro@gmail.com>
2023-05-03 14:20:37 +0200

    fase controllo ciclo aggiunta

--- js-mail/js/mail.js
+++ js-mail/js/mail.js
@@ -4,6 +4,11 @@
 const email = ["marco@gmail.com","luca@gmail.com","carlo@gmail.com","gino@gmail.com","elisa@gmail.com",]
 
 // crea ciclo 
+for (let index = 0; index <email.length; index++) {
+    const contEmail = email[index];
+    console.log(contEmail);
+    
+}
 
     // Chiedi all’utente la sua email
 
