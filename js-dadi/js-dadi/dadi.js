1fc9c062a8fd200ff25d81a3c89efcbccaa6ee60
Zakaria96 <zakarialavoro@gmail.com>
2023-05-03 16:40:02 +0200

    struttura appunti js-dadi

---
+++ js-dadi/js-dadi/dadi.js
@@ -0,0 +1,13 @@
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+// Generare un numero random da 1 a 6 per il giocatore
+
+// Generare un numero random da 1 a 6 per il pc
+
+// confrontare il risultato 
+
+// Stabilire il vincitore 
+
+
+
