e58089aec7f0c963de551986121aa26dd2cec2bc
Zakaria96 <zakarialavoro@gmail.com>
2023-05-03 15:43:57 +0200

    test output

--- js-mail/js/mail.js
+++ js-mail/js/mail.js
@@ -1,15 +1,29 @@
 /*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.*/
 
+// variabili globali 
+const container =document.getElementById("contenitore")
+
+// Chiedi all’utente la sua email
+const emailUser =prompt("inserire email") ;
+let accesoRisultato= "";
+
 // crea lista di chi può accedere
-const email = ["marco@gmail.com","luca@gmail.com","carlo@gmail.com","gino@gmail.com","elisa@gmail.com",]
+const emailList = ["marco@gmail.com","luca@gmail.com","carlo@gmail.com","gino@gmail.com","elisa@gmail.com",]
 
 // crea ciclo 
-for (let index = 0; index <email.length; index++) {
-    const contEmail = email[index];
-    console.log(contEmail);
+for (let i = 0; i < emailList.length; i++) {
+    const contEmail = emailList[i];
+    // console.log(contEmail);
     
-}
-
-    // Chiedi all’utente la sua email
-
+    if (contEmail === emailUser) {  //controlla che sia nella lista di chi può accedere
+        accesoRisultato = "Acceso consentito ";  
+    } else {
+        accesoRisultato = "Acceso negato"
+    }
+    container.innerHTML += `<div class="square">${accesoRisultato}</div>`;
     // crea output 
+}
+console.log(accesoRisultato);
+    
+
+ 
