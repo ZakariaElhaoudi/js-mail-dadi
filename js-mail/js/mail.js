310ab5854a2f247e244de885ca91061066743e5b
Zakaria96 <zakarialavoro@gmail.com>
2023-05-03 14:17:59 +0200

    struttura appunti

---
+++ js-mail/js/mail.js
@@ -0,0 +1,10 @@
+/*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.*/
+
+// crea lista di chi può accedere
+const email = ["marco@gmail.com","luca@gmail.com","carlo@gmail.com","gino@gmail.com","elisa@gmail.com",]
+
+// crea ciclo 
+
+    // Chiedi all’utente la sua email
+
+    // crea output 
