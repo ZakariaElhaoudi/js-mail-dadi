c2d91fc459e46f296f4765e86ae22355746bbe76
Zakaria96 <zakarialavoro@gmail.com>
2023-05-04 15:48:41 +0200

    correzione esercizio

--- js-dadi/js-dadi/dadi.js
+++ js-dadi/js-dadi/dadi.js
@@ -7,15 +7,16 @@ const container =document.getElementById("contenitore")
 let vincitore= "";
 
 
-// Generare un numero random da 1 a 6 per il giocatore
-const giocatore = Math.floor(Math.random() * 6) +1 ;
-console.log("giocatore " + giocatore);
 
-// Generare un numero random da 1 a 6 per il pc
-const computer = Math.floor(Math.random() * 6) +1 ;
-console.log("computer " + computer);
 button.addEventListener('click',
     function () {
+        // Generare un numero random da 1 a 6 per il giocatore
+        const giocatore = Math.floor(Math.random() * 6) +1 ;
+        console.log("giocatore " + giocatore);
+
+        // Generare un numero random da 1 a 6 per il pc
+        const computer = Math.floor(Math.random() * 6) +1 ;
+        console.log("computer " + computer);
         // confrontare il risultato Stabilire il vincitore 
         if (giocatore > computer) {
             vincitore = "Il giocatore ha vinto !!!" ;
